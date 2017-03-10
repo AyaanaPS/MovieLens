a1a167481c61238fc352365a4df8cc30e6f54cb3
Ayaana <ayaanapatelsikora@gmail.com>
2017-03-10 00:19:10 -0800

    Added some comments

--- svd.py
+++ svd.py
@@ -6,10 +6,12 @@ V = np.genfromtxt('V.csv',delimiter=",")
 
 A,S,B = np.linalg.svd(V, full_matrices = False)
 
+# Transpose first 2 columns of A
 plot = np.transpose(A[:,:2])
-
+# Dot product with V to get the projection
 projection = np.dot(plot, V)
 
+# Transpose projection to write to file in columns
 projection = np.transpose(projection)
 
 np.savetxt("coords.csv", projection, delimiter=",")
