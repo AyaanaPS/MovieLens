f7a6fd2167aad80f84f3ea8032f4200c411ab230
Ayaana <ayaanapatelsikora@gmail.com>
2017-03-10 00:18:17 -0800

    Modified SVD

--- svd.py
+++ svd.py
@@ -5,15 +5,11 @@ import matplotlib.pyplot as plt
 V = np.genfromtxt('V.csv',delimiter=",")
 
 A,S,B = np.linalg.svd(V, full_matrices = False)
-print(len(B))
-B = np.transpose(B)
-plot = B[:,:2]
-plot = plot / plot.max(axis=0)
-print plot
 
-movies = {}
-for i,(x,y) in enumerate(plot):
-	movies[i+1] = (x,y)
-	# print(x,y)
+plot = np.transpose(A[:,:2])
 
-np.savetxt("coords.csv", plot, delimiter=",")
+projection = np.dot(plot, V)
+
+projection = np.transpose(projection)
+
+np.savetxt("coords.csv", projection, delimiter=",")
